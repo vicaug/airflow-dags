a4eeb4f2f44687f941c07f84b83cd3028d79f3fe
Victor Augusto <victor_augusto.gabriel@hotmail.com>
2025-07-01 18:23:55 -0300

    innit commit

---
+++ dags/dag_hello.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def hello():
+    print("Hello from Airflow!")
+
+with DAG(
+    'hello_dag',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+    task = PythonOperator(
+        task_id='say_hello',
+        python_callable=hello
+    )
